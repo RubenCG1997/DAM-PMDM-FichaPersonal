5ae1f782a5de5a8a815560935f0e8ee59a234ac9
Ruben <camachogomez1597@gmai.com>
2024-10-28 17:33:12 +0100

    Parte gráfica acabada

--- app/src/main/java/com/example/dam_pmdm_fichapersonal/MainActivity.java
+++ app/src/main/java/com/example/dam_pmdm_fichapersonal/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.dam_pmdm_fichapersonal;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Creamos una variable para saber que curso se ha seleccionado, si es -1 no aparecerá ninguno seleccionado
+    int selectedCourse = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.portrait);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ConstraitLayout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //(findViewById(R.id.btn_info1)).setOnClickListener(v->());
+        (findViewById(R.id.btn_info2)).setOnClickListener(v->elegirCurso());
+        //(findViewById(R.id.btn_info3)).setOnClickListener(v->());
+        //(findViewById(R.id.btn_clear)).setOnClickListener(v->());
+
     }
+
+    private void elegirCurso() {
+
+        final String[] listItems = new String[]{"DAM","DAW","ASIR"};
+
+        new AlertDialog.Builder(MainActivity.this)
+               .setTitle(R.string.titleSC)
+               .setCancelable(false)
+               .setSingleChoiceItems(listItems, selectedCourse, (dialog, which) -> {
+                   selectedCourse = which;
+        })
+                .setNegativeButton(R.string.OptionSC1, null)
+                .setPositiveButton(R.string.OptionSC2, (dialog,which)->mostrarCurso(selectedCourse,listItems))
+                .create()
+                .show();
+    }
+    private void mostrarCurso(int checkedItem,String[] listItems ){
+       if(checkedItem>=0){
+           String cursoSeleccionado = listItems[checkedItem];
+           ((TextView)findViewById(R.id.lbl_info2)).setText(cursoSeleccionado);
+       }
+    }
+
 }
