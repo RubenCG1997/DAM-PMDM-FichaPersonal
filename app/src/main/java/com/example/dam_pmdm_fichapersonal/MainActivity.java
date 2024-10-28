02d037b8649fc04a7a5d5c2b353889bee0c80180
Ruben <camachogomez1597@gmai.com>
2024-10-28 18:56:01 +0100

    Se termina la funcionalidad de los botones "course y "languages"

--- app/src/main/java/com/example/dam_pmdm_fichapersonal/MainActivity.java
+++ app/src/main/java/com/example/dam_pmdm_fichapersonal/MainActivity.java
@@ -1,13 +1,8 @@
 package com.example.dam_pmdm_fichapersonal;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     //Creamos una variable para saber que curso se ha seleccionado, si es -1 no aparecerá ninguno seleccionado
     int selectedCourse = -1;
+    //Creamos una variable para saber cuantos lenguajes hay selecciados.
+
+    boolean[] checkedItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +20,11 @@ public class MainActivity extends AppCompatActivity {
 
         //(findViewById(R.id.btn_info1)).setOnClickListener(v->());
         (findViewById(R.id.btn_info2)).setOnClickListener(v->elegirCurso());
-        //(findViewById(R.id.btn_info3)).setOnClickListener(v->());
+        (findViewById(R.id.btn_info3)).setOnClickListener(v->elegirLenguaje());
         //(findViewById(R.id.btn_clear)).setOnClickListener(v->());
+
+
+
 
     }
 
@@ -42,11 +43,43 @@ public class MainActivity extends AppCompatActivity {
                 .create()
                 .show();
     }
+
     private void mostrarCurso(int checkedItem,String[] listItems ){
        if(checkedItem>=0){
            String cursoSeleccionado = listItems[checkedItem];
            ((TextView)findViewById(R.id.lbl_info2)).setText(cursoSeleccionado);
        }
+    }
+
+    private void elegirLenguaje(){
+        final String [] listItems= new String []{"Java","JavaScript","C#","Kotlin","Python"};
+        checkedItems = new boolean[listItems.length];
+        new AlertDialog.Builder(MainActivity.this)
+            .setTitle(R.string.titleMC)
+                .setCancelable(false)
+                .setMultiChoiceItems(listItems,checkedItems,(dialog,which,isChecked)->{
+                    checkedItems[which] = isChecked;
+                })
+                .setPositiveButton(R.string.Option2MC,(dialog,which)->mostrarLenguaje(checkedItems,listItems))
+                .setNegativeButton(R.string.Option1MC,null)
+                .setNeutralButton(R.string.Optiom3MC,null)
+                .create()
+                .show();
+    }
+
+    private void mostrarLenguaje(boolean[] checkedItems,String[]listItems){
+        String lenguajeSeleccionado = "";
+        for(int i=0;i<=listItems.length-1;i++){
+            if(checkedItems[i]){
+                if(lenguajeSeleccionado.isEmpty()){
+                    lenguajeSeleccionado = listItems[i];
+                }else{
+                    lenguajeSeleccionado += "\n"+ listItems[i] ;
+                }
+
+            }
+        }
+        ((TextView)findViewById(R.id.lbl_info3)).setText(lenguajeSeleccionado);
     }
 
 }
